0b8b4a806b0a9dd7defc3f99051b810d46d02f09
sugarfree10 <2004sugarfree@gmail.com>
2023-09-14 13:58:12 -0700

    creating javascript file

---
+++ index.js
@@ -0,0 +1,26 @@
+let firstName = "Nini";
+let lastName  = "Zedelashvili";
+var birthYear = 2004;
+console.log("my name is " + firstName + ", and surname " + lastName + ". i was born in " + birthYear);
+
+
+var currentYear = 2023;
+var birthYear = 2004;
+var personAge = currentYear - birthYear;
+console.log("the person's age is " + personAge);
+
+
+
+
+// console.log(15 + 7, 30 - 12, 5 * 8, 48 / 6);
+console.log(15 + 7);
+console.log(30 - 12);
+console.log(5 * 8);
+console.log(48 / 6);
+
+
+let weightKg = 120;
+let heightM = 2;
+const BMI = weightKg / (heightM * heightM);
+console.log("your BMI is " + BMI);
+
